e0647345f287e494a64e06c1d730e1a2752ce95d
Agustin <eliasagustin@gmail.com>
2023-06-09 17:56:11 -0300

    Agregue una tiles a la vista Home

--- vistas/home.php
+++ vistas/home.php
@@ -2,6 +2,39 @@
 	<h2 class="subtitle">Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!, espero puedas disfrutar la experiencia en el nuevo modulo llamado "Gestión de Vacantes".</h2>
 </div>
 <br>
+
+<div class="tile is-ancestor">
+  <a class="tile is-parent">
+    <article class="tile is-child box">
+		<br>
+    	<p class="subtitle">Listar Vacantes</p>
+		<br>
+    </article>
+  </a>
+  <a class="tile is-parent">
+    <article class="tile is-child box">
+		<br>
+		<p class="subtitle">Mis Postulaciones</p>
+		<br>
+	</article>
+  </a>
+  <a class="tile is-parent">
+    <article class="tile is-child box">
+		<br>
+		<p class="subtitle">Consultar FAQs</p>
+		<br>
+	</article>
+  </a>
+  <a class="tile is-parent">
+    <article class="tile is-child box">
+		<br>
+		<p class="subtitle">Solicitar Soporte</p>
+		<br>
+	</article>
+  </a>
+</div>
+
+<br>
 <div class="notification is-danger is-light">
     <strong>Tener especial cuidado</strong><br>cuando estoy con una session abierta y fuerzo el login -> podria reloguearme? -> debería corregir el codigo y cuando abre login si estoy logueado me debería llevar a home
 </div>
